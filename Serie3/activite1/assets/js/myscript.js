98c64f84af7e4d42ff507e49c099a8405e99b037
boidinJules <144659808+boidinJules@users.noreply.github.com>
2024-11-21 09:09:42 +0000

    Fin de vérification des données du formulaire

--- Serie3/activite1/assets/js/myscript.js
+++ Serie3/activite1/assets/js/myscript.js
@@ -1,9 +1,12 @@
 function verifierFormulaire() {
-    let baliseNom = document.getElementById('name1');
-    let balisePrénom = document.getElementById("prénom1");
-    let baliseEmail = document.getElementById("email1");
-    let balisePassword = document.getElementById("password1");
-    let baliseMessage = document.getElementById("message1");
+    const baliseNom = document.getElementById('name1');
+    const balisePrénom = document.getElementById("prénom1");
+    const baliseEmail = document.getElementById("email1");
+    const balisePassword = document.getElementById("password1");
+    const BaliseErrorpassword = document.querySelector('.form-group small');
+    const baliseMessage = document.getElementById("message1");
+    const baliseCheckbox = document.getElementById('formCheck-1');
+    const baliseLabel = document.getElementById('formCheckLabel');
 
     if (baliseNom.value == '') {
         baliseNom.style.borderColor = "red";
@@ -17,22 +20,32 @@ function verifierFormulaire() {
         balisePrénom.style.borderColor = "";
     }
 
-    if (baliseEmail.value.length < 7) {
+    if (baliseEmail.value == '' || !baliseEmail.value.includes('@')) {
         baliseEmail.style.borderColor = "red";
     } else {
         baliseEmail.style.borderColor = "";
     }
 
-    if (balisePassword.value.length < 8) {
+    if (balisePassword.value == '') {
+        balisePassword.style.borderColor = "red";
+    } else if (balisePassword.value.length < 8){
+        BaliseErrorpassword.classList.remove('invisible');
         balisePassword.style.borderColor = "red";
     } else {
         balisePassword.style.borderColor = "";
+        BaliseErrorpassword.classList.add('invisible');
     }
 
     if (baliseMessage.value == '') {
         baliseMessage.style.borderColor = "red";
     } else {
         baliseMessage.style.borderColor = "";
+    }
+
+    if (baliseCheckbox.checked) {
+        baliseLabel.style.color = "";
+    } else {
+        baliseLabel.style.color = "red";
     }
 
 }
